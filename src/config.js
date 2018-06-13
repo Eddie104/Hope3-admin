b6ef3f0bd316df02ecd29d428d95908fabb015cc
hongjie <32968210@qq.com>
2018-06-13 22:50:44 +0800

    图片服务器改成七牛的

--- src/config.js
+++ src/config.js
@@ -8,6 +8,7 @@ export const CLIENT_SECRET = 'A5w3R2DluABnFoBqAXksclt5vbhUTCdBslUTmzjCKBF';
 // 七牛空间的域名
 export const QINIU_DOMAIN = 'http://p5epqbgwe.bkt.clouddn.com';
 
-export const IMG_SERVER = 'http://47.100.164.90/Hope3';
+// export const IMG_SERVER = 'http://47.100.164.90/Hope3';
+export const IMG_SERVER = 'http://pa9m48qrj.bkt.clouddn.com';
 
 export const GENDER = ['通用', '男人', '女人', '男童', '女童'];
