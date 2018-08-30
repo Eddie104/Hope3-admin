bc2e0a2f371c9bd07a7d0b8b5f0898807c350a41
hongjie <32968210@qq.com>
2018-08-30 22:34:36 +0800

    更换服务器地址

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL = 'http://localhost:7001';
-export const SERVER_URL = 'http://47.100.164.90:7004';
+export const SERVER_URL = 'http://106.15.93.73:7004';
 
 export const CLIENT_ID = 'hopeadmin';
 
@@ -8,7 +8,7 @@ export const CLIENT_SECRET = 'A5w3R2DluABnFoBqAXksclt5vbhUTCdBslUTmzjCKBF';
 // 七牛空间的域名
 export const QINIU_DOMAIN = 'http://p5epqbgwe.bkt.clouddn.com';
 
-// export const IMG_SERVER = 'http://47.100.164.90/Hope3';
+// export const IMG_SERVER = 'http://106.15.93.73/Hope3';
 export const IMG_SERVER = 'http://pa9m48qrj.bkt.clouddn.com';
 
 export const GENDER = ['通用', '男人', '女人', '男童', '女童'];
