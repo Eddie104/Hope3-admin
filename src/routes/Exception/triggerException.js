bfa3315646cdfae9f628ba441f4156e6529cfb9e
hongjie <32968210@qq.com>
2018-09-16 22:06:58 +0800

    clean up

--- src/routes/Exception/triggerException.js
+++ src/routes/Exception/triggerException.js
@@ -4,49 +4,49 @@ import { connect } from 'dva';
 import styles from './style.less';
 
 @connect(state => ({
-  isloading: state.error.isloading,
+    isloading: state.error.isloading,
 }))
 export default class TriggerException extends PureComponent {
-  state={
-    isloading: false,
-  }
-  trigger403 = () => {
-    this.setState({
-      isloading: true,
-    });
-    this.props.dispatch({
-      type: 'error/query403',
-    });
-  };
-  trigger500 = () => {
-    this.setState({
-      isloading: true,
-    });
-    this.props.dispatch({
-      type: 'error/query500',
-    });
-  };
-  trigger404 = () => {
-    this.setState({
-      isloading: true,
-    });
-    this.props.dispatch({
-      type: 'error/query404',
-    });
-  };
-  render() {
-    return (
-      <Spin spinning={this.state.isloading} wrapperClassName={styles.trigger}>
-        <Button type="danger" onClick={this.trigger403}>
+    state={
+        isloading: false,
+    }
+    trigger403 = () => {
+        this.setState({
+            isloading: true,
+        });
+        this.props.dispatch({
+            type: 'error/query403',
+        });
+    };
+    trigger500 = () => {
+        this.setState({
+            isloading: true,
+        });
+        this.props.dispatch({
+            type: 'error/query500',
+        });
+    };
+    trigger404 = () => {
+        this.setState({
+            isloading: true,
+        });
+        this.props.dispatch({
+            type: 'error/query404',
+        });
+    };
+    render() {
+        return (
+            <Spin spinning={this.state.isloading} wrapperClassName={styles.trigger}>
+                <Button type="danger" onClick={this.trigger403}>
         触发403
-        </Button>
-        <Button type="danger" onClick={this.trigger500}>
+                </Button>
+                <Button type="danger" onClick={this.trigger500}>
          触发500
-        </Button>
-        <Button type="danger" onClick={this.trigger404}>
+                </Button>
+                <Button type="danger" onClick={this.trigger404}>
          触发404
-        </Button>
-      </Spin>
-    );
-  }
+                </Button>
+            </Spin>
+        );
+    }
 }
