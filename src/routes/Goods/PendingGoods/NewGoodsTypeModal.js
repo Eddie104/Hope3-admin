24389b6e5d87eec429ad70eb737c25f04599f93f
hongjie <32968210@qq.com>
2018-11-13 20:35:06 +0800

    fixed a bug

--- src/routes/Goods/PendingGoods/NewGoodsTypeModal.js
+++ src/routes/Goods/PendingGoods/NewGoodsTypeModal.js
@@ -29,7 +29,7 @@ export default class NewGoodsTypeModal extends PureComponent {
             category: nextProps.category,
             subCategory: nextProps.subCategory,
             visible: !!nextProps.visible,
-            pendingGoods: { ...nextProps.pendingGoods, ...this.state.pendingGoods },
+            pendingGoods: { ...nextProps.pendingGoods },
             isShowingSpin: false,
         });
     }
