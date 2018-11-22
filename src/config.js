dab5443ca8a394734121acc76d589fff84319e57
hongjie <32968210@qq.com>
2018-11-22 23:35:11 +0800

    换了新的域名

--- src/config.js
+++ src/config.js
@@ -6,9 +6,10 @@ export const CLIENT_ID = 'hopeadmin';
 export const CLIENT_SECRET = 'A5w3R2DluABnFoBqAXksclt5vbhUTCdBslUTmzjCKBF';
 
 // 七牛空间的域名
-export const QINIU_DOMAIN = 'http://p5epqbgwe.bkt.clouddn.com';
+// export const QINIU_DOMAIN = 'http://p5epqbgwe.bkt.clouddn.com';
+export const QINIU_DOMAIN = 'http://hope3.pksen.com';
 
-// export const IMG_SERVER = 'http://106.15.93.73/Hope3';
-export const IMG_SERVER = 'http://pa9m48qrj.bkt.clouddn.com';
+// export const IMG_SERVER = 'http://pa9m48qrj.bkt.clouddn.com';
+export const IMG_SERVER = 'http://hope3.pksen.com';
 
 export const GENDER = ['通用', '男人', '女人', '男童', '女童'];
